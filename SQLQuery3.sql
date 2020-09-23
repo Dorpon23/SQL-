7ba5646e010bc156bea78bfe40c4ff155e4a5b6b
unknown <dorpon_se@yahoo.com>
2020-09-23 10:44:05 +0600

    Temparary table

--- SQLQuery3.sql
+++ SQLQuery3.sql
@@ -31,17 +31,13 @@ Foreign Key (DepartmentId) references tblDepartment(Id)
 Select*from tblEmploye_Info3
 Select*from tblDepartment
 
-/*Temporary table create in datadabase*/
-Select Name,GenderId,salary Into #tblEmploye_Info3 From tblEmploye_Info3
-Select*from #tblEmploye_Info3
-delete from #tblEmploye_Info3 where Name='Tom'
-Drop TABLE #tblEmploye_Info3
-
 
 Select sum(salary) as Total_salary from tblEmploye_Info3
 Select Min(salary) as Minimum_salary from tblEmploye_Info3
 Select Max(salary) as Maximum_salary from tblEmploye_Info3
+
 Select city, SUM(salary) as Total_salary from tblEmploye_Info3 Group by city
+
 Select city,GenderId, SUM(salary) as Total_salary 
 from tblEmploye_Info3 
 Group by city,GenderId order by city Desc
@@ -67,6 +63,23 @@ order by city Desc
 -- 'WHERE' is using before Group by
 --  'HAVING' IS USING AFTER 'GROUP BY' AND 'ODER BY' is using after 'HAVING'
 
+/*Temporary table create in datadabase*/
+Select Name,GenderId,salary Into #tblEmploye_Info3 From tblEmploye_Info3
+Select*from #tblEmploye_Info3
+Select*from #tblEmploye_Info3 where Name ='Tom'
+delete from #tblEmploye_Info3 where Name='Tom'
+Drop TABLE  #tblEmploye_Info3
+
+select Name,DepartmentId from  tblEmploye_Info3 where DepartmentId Is Null
+
+select Id,Name,city,salary,DepartmentId from tblEmploye_Info3 where  DepartmentId =1 or DepartmentId=2 And salary>4000
+select Id,Name,city,salary,DepartmentId from tblEmploye_Info3 where (DepartmentId =1 or DepartmentId=2) And salary>4000
+--Why is it happening?
+--Because,AND OPERATION always do the work before OR.
+--SO,This reason use paranthes()
+select * from tblEmploye_Info3 where not city='London' and not city='New York'
+
+
 --Insert into tblEmploye_Info3 (Name,GenderId,salary,City) Values('Tom','Male',4800,'London')
 --Insert into tblEmploye_Info3 (Name,GenderId,salary,City) Values('Pam','Feale',3000,'New York')
 --Insert into tblEmploye_Info3 (Name,GenderId,salary,City) Values('John','Male',3500,'London')
@@ -76,11 +89,7 @@ order by city Desc
 --Insert into tblEmploye_Info3 (Name,GenderId,salary,City) Values('Sara','Male',4800,'Sydney')
 --Insert into tblEmploye_Info3 (Name,GenderId,salary,City) Values('Valane','Female',5500,'New York')
 --Insert into tblEmploye_Info3 (Name,GenderId,salary,City) Values('James','Female',6500,'London')
-Insert into tblEmploye_Info3 (Name,GenderId,salary,City) Values('Russel','Male',9800,'London')
-
-
-
-select * from tblDepartment
+--Insert into tblEmploye_Info3 (Name,GenderId,salary,City) Values('Russel','Male',9800,'London')
 
 
 
